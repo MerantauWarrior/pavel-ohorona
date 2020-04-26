478a2fef6b9eafeee6a7a0f92e9cf47fb87ffeb7
Boris <boris1488ss@gmail.com>
2020-04-27 00:56:21 +0300

    all pages responsive

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,13 @@
-// $(document).keydown(function (event) {
-//   if (event.keyCode == 123) { // Prevent F12
-//     return false;
-//   } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) { // Prevent Ctrl+Shift+I
-//     return false;
-//   }
-// });
-// $(document).on("contextmenu", function (e) {
-//   e.preventDefault();
-// });
+$(document).keydown(function (event) {
+  if (event.keyCode == 123) {
+    return false;
+  } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
+    return false;
+  }
+});
+$(document).on("contextmenu", function (e) {
+  e.preventDefault();
+});
 
 $( document ).ready(function() {
 
